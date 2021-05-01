72de5ca9abfe35379308db81717e3a18a2f804ce
Vamiq <vamiq.valji@gmail.com>
2021-04-30 23:00:56 -0400

    added users create route and error checking for that route

--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import users from "../models/usersModel";
+import {usersInterface} from "../ts/interface";
+
 const router = express.Router();
 
 router.get("/view", async (req, res) => {
@@ -10,17 +12,24 @@ router.get("/view", async (req, res) => {
     return res.status(200).json({success: true, message: "Returning all users", data: foundUsers});
 });
 
-router.post("/", async (req, res) => {
+router.post("/create", async (req, res) => {
 
-    const { inputName } = req.body;
+    const { username, password }:usersInterface = req.body;
     
+    const isDuplicate = await users.findOne({ username });
+    if (isDuplicate) return res.status(400).json({success: false, message: "Account already exists."});
+
+    const valuesAreValid = username && password;
+    if (!valuesAreValid) return res.status(400).json({success: false, message: "Invalid values. Required: username, password"});
+
     let newUser:mongoose.Document = new users({
-        name: "placeholder",
+        username,
+        password,
     });
 
     await newUser.save();
 
-    return res.status(201).json({success: true, message: `Created user: '${inputName}'.`});
+    return res.status(201).json({success: true, message: `Created user: '${username}'.`});
     // return res.status(400).json({success: false, message: "Error in creation."});
 });
 
