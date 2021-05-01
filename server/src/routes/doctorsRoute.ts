20c3e89ca1f2bb25a3b5ecf53aed7d01e0833f77
Vamiq <vamiq.valji@gmail.com>
2021-04-30 22:32:51 -0400

    added 'GET' routes to doctors and users routes

--- server/src/routes/doctorsRoute.ts
+++ server/src/routes/doctorsRoute.ts
@@ -3,18 +3,25 @@ import mongoose from "mongoose";
 import doctors from "../models/doctorsModel";
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    const foundDoctors = await doctors.find({});
+
+    if (!foundDoctors) return res.status(400).json({success: false, data: "Error in fetching."});
+    return res.status(200).json({success: true, message: "Returning all doctors", data: foundDoctors});
+});
+
 router.post("/", async (req, res) => {
 
     const { inputName } = req.body;
     
-        let newDoctor:mongoose.Document = new doctors({
-            name: "placeholder",
-        });
-    
-        await newDoctor.save();
-    
-        return res.status(201).json({success: true, message: `Created listing for '${inputName}'.`});
-        // return res.status(400).json({success: false, message: "Error in creation."});
+    let newDoctor:mongoose.Document = new doctors({
+        name: "placeholder",
+    });
+
+    await newDoctor.save();
+
+    return res.status(201).json({success: true, data: `Created listing for '${inputName}'.`});
+    // return res.status(400).json({success: false, data: "Error in creation."});
 });
 
 export default router;
--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -3,18 +3,25 @@ import mongoose from "mongoose";
 import users from "../models/usersModel";
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    const foundUsers = await users.find({});
+
+    if (!foundUsers) return res.status(400).json({success: false, data: "Error in fetching."});
+    return res.status(200).json({success: true, message: "Returning all users", data: foundUsers});
+});
+
 router.post("/", async (req, res) => {
 
     const { inputName } = req.body;
     
-        let newUser:mongoose.Document = new users({
-            name: "placeholder",
-        });
-    
-        await newUser.save();
-    
-        return res.status(201).json({success: true, message: `Created user: '${inputName}'.`});
-        // return res.status(400).json({success: false, message: "Error in creation."});
+    let newUser:mongoose.Document = new users({
+        name: "placeholder",
+    });
+
+    await newUser.save();
+
+    return res.status(201).json({success: true, message: `Created user: '${inputName}'.`});
+    // return res.status(400).json({success: false, message: "Error in creation."});
 });
 
 export default router;
