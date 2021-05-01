172ea80cd2c902686e89c1f4379a87c691c9fe4f
Vamiq <vamiq.valji@gmail.com>
2021-05-01 15:36:46 -0400

    sends back account data on account creation

--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -44,7 +44,7 @@ router.post("/create", async (req, res) => {
 
     await newUser.save();
 
-    return res.status(201).json({success: true, message: `Created user: '${username}'.`});
+    return res.status(201).json({success: true, message: `Created user: '${username}'.`, data: newUser});
     // return res.status(400).json({success: false, message: "Error in creation."});
 });
 
