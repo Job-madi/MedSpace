3056ae37a04509f726172dfc4ccab3fc292e0d19
Job-madi <job.madi@protonmail.com>
2021-05-02 21:33:54 +0200

    account model class

---
+++ flutter_app/medspace/lib/models/account.dart
@@ -0,0 +1,12 @@
+class AccountInfo{
+
+
+ final String username;
+ String name;
+ String surname;
+ dynamic picture;
+ String country;
+
+ AccountInfo({this.username, this.name, this.surname, this.picture, this.country});
+ 
+}
